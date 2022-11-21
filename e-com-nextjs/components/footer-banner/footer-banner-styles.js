f8016efa450200228ee879e6381287a5a44ecd2a
lss555 <chrisheibel7@gmail.com>
2022-11-21 01:12:46 -0700

    footer banner styling

--- e-com-nextjs/components/footer-banner/footer-banner-styles.js
+++ e-com-nextjs/components/footer-banner/footer-banner-styles.js
@@ -6,37 +6,65 @@ export const FooterBannerContainer = styled.div`
 	height: 30vh;
 	width: 100%;
 	display: flex;
-	margin-top: 2rem;
+	margin: 2rem auto auto auto;
 	max-width: 1800px;
-	margin-right: auto;
-	margin-left: auto;
+	
 `;
 
 export const LogoImage = styled.img`
 	display: flex;
 	width: 100%;
 	height: 30vh;
-	
 	object-fit: cover;
 	
 `;
 
 export const FooterBannerCard = styled.div`
-	diplay: flex;
-	height: 15vh;
+	
+	height: 30vh;
 	width: 100%;
 	:hover {
-		opacity: .6;
+		opacity: 1;
 	}
 `;
 
 export const FooterBannerName = styled.h1`
 	display: flex;
+	flex-direction: column;
 	color: white;
 	position: absolute;
 	margin-left: 2rem;
 `;
 
 export const TextContainer = styled.div`
+	display:
+	flex-direction: column;
+	height: auto;
+	width: 100%;
+	margin: auto;
+`;
+
+export const FooterBannerButton = styled.button`
+	display: flex;
+	flex-direction: column;
+	position: absolute;
+	margin: 6rem auto auto 2rem;
+	padding: .8rem;
+	font-size: 1rem;
+	background-color: black;
+	opacity: .8;
+	border: 2px solid #f12307;
+	border-radius: 5px;
+	color: white;
+
+	:hover {
+		opacity: 1;
+		color: #f12307;
+		transition: all .7s ease;
+		transform: scale(1.1);
+	}
+`;
+
+export const FooterBannerButtonContainer = styled.div`
 	
 `;
--- e-com-nextjs/components/footer-banner/footer-banner.jsx
+++ e-com-nextjs/components/footer-banner/footer-banner.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { FooterBannerContainer, LogoImage, FooterBannerCard, FooterBannerName, TextContainer } from './footer-banner-styles.js';
+import { FooterBannerContainer, LogoImage, FooterBannerCard, FooterBannerName, TextContainer, FooterBannerButton, FooterBannerButtonContainer } from './footer-banner-styles.js';
 import { urlFor } from '../../lib/client';
 
 // <LogoImage src={urlFor(image && image[0])} width={250} height={250} />
 
-const FooterBanner = ({ footerBanner }) => {
+const FooterBanner = ({ footerBanner: { title, image } }) => {
 
 	return (
 		<FooterBannerContainer>
 			<FooterBannerCard>
 				<TextContainer>
-					<FooterBannerName>{footerBanner.title}</FooterBannerName>
+					<FooterBannerName>{title}</FooterBannerName>
+					<FooterBannerButtonContainer>
+						<FooterBannerButton>Explore</FooterBannerButton>
+					</FooterBannerButtonContainer>
 				</TextContainer>
-				<LogoImage src={urlFor(footerBanner.image && footerBanner.image[0])} width={250} height={250} />
+				<LogoImage src={urlFor(image && image[0])} width={250} height={250} />
 			</FooterBannerCard>
 		</FooterBannerContainer>
 		);
